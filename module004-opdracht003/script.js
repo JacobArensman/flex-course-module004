dfb0611c59f7c1996634d94bd5c315bb5aef72f4
JacobArensman <66920417+JacobArensman@users.noreply.github.com>
2020-07-27 12:55:39 +0200

    Add files via upload

---
+++ module004-opdracht003/script.js
@@ -0,0 +1,58 @@
+
+//A
+const addTheWordCool = "Cool" 
+
+	addFunction = () => {
+}
+console.log(addTheWordCool, "nice", "awesome", "tof");
+//----------------------------------------------------------------------
+let amountOfElementsInArray = ['appels', 'peren', 'citroenen'];
+
+console.log(amountOfElementsInArray.length); // 3
+
+
+
+//-----------------------------------------------------------------------
+
+const selectBelgiumFromBenelux = ["Belgie", "Nederland", "Luxemburg"];
+
+console.log(selectBelgiumFromBenelux[0]); 
+// resultaat: "Belgie"
+
+
+//-----------------------------------------------------------------------
+
+const lastElementInArray =  ["Haas", "Cavia", "Kip", "Schildpad"];
+
+console.log(lastElementInArray[3]); 
+// resultaat: "Schildpad"
+
+
+
+//----------------------------------------------------------------------
+
+
+const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+
+const impeachTrumpSlice = function(array) {
+(presidents = impeachTrumpSlice.slice[0])
+}
+const impeachTrumpSplice = function(array) {
+	(presidents = impeachTrumpSplice(pos, 0))
+}
+
+console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]
+console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]
+
+//--------------------------------------------------------------------------
+
+const stringsTogether = ['Winc', 'Academy', 'is', 'leuk', ';-}'];
+
+console.log(stringsTogether.join(' ')) 
+//resultaat: "Winc Academy is leuk ;-}"
+
+//-------------------------------------------------------------------------
+
+let arr1 = [1,2,3];
+let arr2 = [4,5,6];
+console.log (arr1.concat(arr2));
