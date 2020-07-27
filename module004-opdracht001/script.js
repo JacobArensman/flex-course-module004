4b3e8c1b0587653c09e8327003ab21291851ab39
JacobArensman <66920417+JacobArensman@users.noreply.github.com>
2020-07-27 12:20:18 +0200

    Add files via upload

---
+++ module004-opdracht001/script.js
@@ -0,0 +1,22 @@
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+  
+  ikRockArrowFunctions()
+
+const fivePlusSeven = (a, b) => a + b
+console.log(fivePlusSeven(5, 7));
+
+console.log(1 + 2)
+
+const myFunction = (a, b) => a + b
+console.log(myFunction(5, 7));
+
+let addFive = (a) => a + 5
+
+console.log(addFive(10));
+
+const createObject = (greeting) => {
+    greeting = 'Geachte';
+    console.log(createObject(greeting));
+  };
